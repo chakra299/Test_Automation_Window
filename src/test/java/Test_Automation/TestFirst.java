ecfa085fef763522497f72d5c5848949ba422a20
chakra299 <chakra299@gmail.com>
2026-04-25 10:30:10 -0700

    First Commit

---
+++ src/test/java/Test_Automation/TestFirst.java
@@ -0,0 +1,14 @@
+package Test_Automation;
+
+import org.testng.annotations.Test;
+
+public class TestFirst {
+	
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("its git testing");
+	}
+
+}
